d4dfb56637ce692e6f0514138ddbbe98cf0cae4b
fahmid <fahmidhasankonok@gmail.com>
2022-11-05 02:50:06 +0600

    add gmap

--- components/workprocess.vue
+++ components/workprocess.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 export default {
-    name='workprocess',
+    name:'workprocess',
 }
 </script>
 <style lang="">
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,23 @@
 <template lang="">
-    <div>
-        <h1>contact</h1>
+    <div class='container'>
+        <div class="row">
+          <div class='col-md-12 p-3'>
+          <h2 class='text-center'>Contact us</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3649.7049235869845!2d90.41789591478566!3d23.82908968455061!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c72c6238452f%3A0x5b1b58ce3e37a804!2sKhilkhet%20Bus%20stop!5e0!3m2!1sen!2sbd!4v1667594069388!5m2!1sen!2sbd" 
+            width="100%" 
+            height="450" 
+            class='p-1 m-1'
+            style="border:0;" 
+            allowfullscreen="" 
+            loading="lazy" 
+            referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+          </div>
+        </div>
     </div>
 </template>
 <script>
